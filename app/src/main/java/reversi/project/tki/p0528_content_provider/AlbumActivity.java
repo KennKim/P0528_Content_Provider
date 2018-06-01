35b51c1c7d7b1eb1a54e7a80e80634aaa2847150
Deviation <dnf126@gmail.com>
2018-06-01 16:02:18 +0900

    linkedHashMap 테스트

--- app/src/main/java/reversi/project/tki/p0528_content_provider/AlbumActivity.java
+++ app/src/main/java/reversi/project/tki/p0528_content_provider/AlbumActivity.java
@@ -8,9 +8,11 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 import reversi.project.tki.p0528_content_provider.databinding.ActivityAlbumBinding;
 import reversi.project.tki.p0528_content_provider.util.MyTime;
@@ -21,34 +23,31 @@ public class AlbumActivity extends AppCompatActivity {
 
     private AlbumAdapter mAdapter;
     private ArrayList<Photo> itemsAll = new ArrayList<>();
-    //    public static ArrayList<Photo> items = new ArrayList<>();
-    public static ArrayList<Uri> items = new ArrayList<>();
-
-    //    private ArrayAdapter<String> spinAdapter;
+    private ArrayList<Photo> items = new ArrayList<>();
+    private LinkedHashMap<String, ArrayList<Photo>> mapFolder = new LinkedHashMap<>();
     private ArrayList<String> listFolder = new ArrayList<>();
-
-    public static String folder;
-    private int folderIndex = 0;
-
-
-    private ArrayList<Photo> itemsFolder = new ArrayList<>();
-    private ArrayList<PhotoFolder> listPhotoFolder = new ArrayList<>();
-
+    private int spinIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         b = DataBindingUtil.setContentView(this, R.layout.activity_album);
         b.setActivity(this);
 
         b.progress.setVisibility(View.VISIBLE);
-//        getThumbAll();
         fetchAllImages();
+        modifyFolderName();
 
+        initView();
 
-        mAdapter = new AlbumAdapter(this, itemsAll,
+        b.spin.setSelection(spinIndex);
+//        mAdapter.notifyDataSetChanged();
+        b.progress.setVisibility(View.INVISIBLE);
+
+    }
+
+    private void initView() {
+        mAdapter = new AlbumAdapter(this, items,
                 new AlbumAdapter.OnSelectListener() {
                     @Override
                     public void onSelected(Photo photo) {
@@ -66,7 +65,7 @@ public class AlbumActivity extends AppCompatActivity {
                 });
         b.rv.setAdapter(mAdapter);
 
-        /*ArrayAdapter<String> spinAdapter = new ArrayAdapter<>(AlbumActivity.this, android.R.layout.simple_spinner_item, listFolder);
+        ArrayAdapter<String> spinAdapter = new ArrayAdapter<>(AlbumActivity.this, android.R.layout.simple_spinner_item, listFolder);
         spinAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         b.spin.setAdapter(spinAdapter);
@@ -75,7 +74,8 @@ public class AlbumActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
                 Log.d("tttest", "onItemSelected");
-                mAdapter.notifyDataSetChanged();
+//                mAdapter.notifyDataSetChanged();
+//                items = mapFolder.values().toArray()[position];
             }
 
             @Override
@@ -84,18 +84,9 @@ public class AlbumActivity extends AppCompatActivity {
 
             }
         });
-*/
-
-
-        mAdapter.notifyDataSetChanged();
-//        b.iv.setImageURI(itemsAll.get(10).thumbUri);
-        b.progress.setVisibility(View.INVISIBLE);
-
     }
 
     private void fetchAllImages() {
-
-
         // DATA는 이미지 파일의 스트림 데이터 경로를 나타냅니다.
         String[] projection = {
                 MediaStore.Images.Media.DATA,
@@ -133,214 +124,59 @@ public class AlbumActivity extends AppCompatActivity {
                     String id = cursor.getString(dataId);
                     String size = cursor.getString(datasize);
                     String orientation = cursor.getString(dataorientation);
-//                    String contenttype = cursor.getString(dataContentType);
-
                     Uri imageUri = Uri.parse(filePath);
 
-
                     //todo: photo pojo 정리하고, folder 별로 spinner 해결할 것. 그리고 GLIDE uri로 load 하는거 찾아볼 것. 꼭 file로만 해야 하는건지?
-
 
                     String taken = cursor.getString(dataDateTaken);
                     String takenDate = MyTime.getStringFormat(taken, MyTime.DATE_FORMAT);
 
 
                     Photo photo = new Photo(imageUri, filePath, id, taken, size, orientation);
-                    itemsAll.add(photo);
+//                    itemsAll.add(photo);
 
-
-//                    addListFolder(photo);
-
-
-                    /*
-
-                    String filePath = cursor.getString(dataColumnIndex);
-                    Uri imageUri = Uri.parse(filePath);
-                    items.add(imageUri);*/
+                    addListFolder(photo);
 
                 } while (cursor.moveToNext());
 
             } else {
-                // imageCursor가 비었습니다.
+                // Cursor가 비었습니다.
             }
         }
         cursor.close();
     }
 
-    //    private String currentFolder = null;
-    private ArrayList<Photo> listTemp = new ArrayList<>();
-
     private void addListFolder(Photo photo) {
-
         String[] a = photo.photoPath.split("/");
         String folder = a[a.length - 2];
-
 
         if (folder == null) {
             return;
         }
 
         if (listFolder.contains(folder)) {    // folder가 있을때 case1. photo를 추가해야 함.
-
-            listTemp.add(photo);
+            ArrayList<Photo> list = mapFolder.get(folder);
+            list.add(photo);
+            mapFolder.put(folder, list);
 
         } else {                              // folder가 없을때 case1.처음 case2.다음 folder
-
-//            currentFolder = folder;
             listFolder.add(folder);
-
-
-            if (!listTemp.isEmpty()) {
-                int size = listTemp.size();
-                String modifiedFolderName = folder + " (" + size + ")";
-                listPhotoFolder.add(new PhotoFolder(modifiedFolderName, listTemp));
-
-                listTemp.clear();
-                listTemp.add(photo);
-            }
-            listTemp.add(photo);
-
+            ArrayList<Photo> list = new ArrayList<>();
+            list.add(photo);
+            mapFolder.put(folder, list);
 
         }
-        /*
-        if (listFolder.contains(folder)) {    // folder가 있을때 case1. photo를 추가해야 함.
 
-            listTemp.add(photo);
-
-        } else {                              // folder가 없을때 case1.처음 case2.다음 folder
-
-//            currentFolder = folder;
-            listFolder.add(folder);
-
-            if (!listTemp.isEmpty()) {
-                int size = listTemp.size();
-                String modifiedFolderName = folder + " (" + size + ")";
-                listPhotoFolder.add(new PhotoFolder(modifiedFolderName, listTemp));
-
-                listTemp.clear();
-                listTemp.add(photo);
-            }
-            listTemp.add(photo);
-
-
-        }*/
     }
 
     private void modifyFolderName() {
         listFolder.clear();
-        for (PhotoFolder pf : listPhotoFolder) {
-            listFolder.add(pf.folder);
-        }
-
-       /*     int size = pf.items.size();
-
-            int i =0;
-            for (String folder : listFolder){
-                if (folder.equals(pf.folder)) {
-                    String folderAddedCount = folder + " (" + size + ")";
-                    listFolder.set(i, folderAddedCount);
-                }
-                i++;
-            }
-        }*/
-    }
-
-    private Uri getThumbUri(String imageId) {
-        // DATA는 이미지 파일의 스트림 데이터 경로를 나타냅니다.
-        String[] projection = {MediaStore.Images.Thumbnails.DATA};
-
-        // 원본 이미지의 _ID가 매개변수 imageId인 썸네일을 출력
-        Cursor cursor = getContentResolver().query(
-                MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, // 썸네일 컨텐트 테이블
-                projection, // DATA를 출력
-                MediaStore.Images.Thumbnails.IMAGE_ID + "=?", // IMAGE_ID는 원본 이미지의 _ID를 나타냅니다.
-                new String[]{imageId},
-                null);
-
-
-        if (cursor != null && cursor.moveToFirst()) {
-
-            int thumbnailColumnIndex = cursor.getColumnIndex(projection[0]);
-            String thumbnailPath = cursor.getString(thumbnailColumnIndex);
-            cursor.close();
-            return Uri.parse(thumbnailPath);
-        } else {
-            // thumbnailCursor가 비었습니다.
-            // 이는 이미지 파일이 있더라도 썸네일이 존재하지 않을 수 있기 때문입니다.
-            // 보통 이미지가 생성된 지 얼마 되지 않았을 때 그렇습니다.
-            // 썸네일이 존재하지 않을 때에는 아래와 같이 썸네일을 생성하도록 요청합니다
-            MediaStore.Images.Thumbnails.getThumbnail(getContentResolver(),
-                    Long.parseLong(imageId),
-                    MediaStore.Images.Thumbnails.MINI_KIND,
-                    null);
-            cursor.close();
-            return getThumbUri(imageId);
-/*
-
-Bitmap bitmap = MediaStore.Images.Thumbnails.getThumbnail(getContentResolver(),
-	 selectedImageUri,
-	MediaStore.Images.Thumbnails.MINI_KIND,
-	 null );
-*/
+        for (String f : mapFolder.keySet()) {
+            int size = mapFolder.get(f).size();
+            listFolder.add(f + " (" + size + ")");
 
         }
     }
 
-    private Uri getTtestThumbUri(String id) {
-        Uri thumbUri = null;
-
-        String[] projection = {MediaStore.Images.Thumbnails.DATA};
-
-        Cursor cursor = MediaStore.Images.Thumbnails.queryMiniThumbnail(
-                getContentResolver(),
-                Long.parseLong(id),
-                MediaStore.Images.Thumbnails.MINI_KIND,
-                null);
-        if (cursor != null && cursor.moveToFirst()) {
-
-            int thumbnailColumnIndex = cursor.getColumnIndex(projection[0]);
-            String thumbnailPath = cursor.getString(thumbnailColumnIndex);
-            thumbUri = Uri.parse(thumbnailPath);
-        }
-
-        return thumbUri;
-    }
-
-
-    private void getThumbAll() {
-        // DATA는 이미지 파일의 스트림 데이터 경로를 나타냅니다.
-        String[] projection = {MediaStore.Images.Thumbnails.DATA};
-
-        // 원본 이미지의 _ID가 매개변수 imageId인 썸네일을 출력
-        Cursor cursor = getContentResolver().query(
-                MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, // 썸네일 컨텐트 테이블
-                projection, // DATA를 출력
-                null, // IMAGE_ID는 원본 이미지의 _ID를 나타냅니다.
-                null,
-                null);
-
-
-        if (cursor == null) {
-
-            Toast.makeText(AlbumActivity.this, "null", Toast.LENGTH_SHORT).show();
-
-        } else {
-
-            int thumbnailColumnIndex = cursor.getColumnIndex(projection[0]);
-
-            if (cursor.moveToFirst()) {
-                do {
-                    String thumbnailPath = cursor.getString(thumbnailColumnIndex);
-                    Uri thumbnailUri = Uri.parse(thumbnailPath);
-
-                    items.add(thumbnailUri);
-
-                } while (cursor.moveToNext());
-
-            }
-        }
-        cursor.close();
-
-    }
 
 }
